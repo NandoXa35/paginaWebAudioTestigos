f6939eea015befa6d1aafc9348fc7a66b415b435
NandoXa35 <nando300515@gmail.com>
2025-05-23 11:52:24 -0600

    subir archivos estaticos

--- pergoproject/settings.py
+++ pergoproject/settings.py
@@ -63,7 +63,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web-production-ef96f.up.railway.app'
                  ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://*'
+    'http://*','https://web-production-ef96f.up.railway.app'
 
 ]
 
@@ -164,7 +164,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 LOGIN_URL = '/signin'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
