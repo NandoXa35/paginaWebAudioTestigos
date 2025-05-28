4989207a90326865499754f40522fd1601c3da23
NandoXa35 <nando300515@gmail.com>
2025-05-28 13:02:26 -0600

    Version publica v3

--- pergoapp/views.py
+++ pergoapp/views.py
@@ -162,7 +162,7 @@ def Downloads(request):
     client = storage.Client(credentials=settings.GS_CREDENTIALS)
     bucket = settings.GCP_BUCKET_NAME
 
-    blob_name = "https://storage.googleapis.com/datos_web_audio_testigos/prorgramas/GenAudTes_Installer.exe"  # ruta dentro del bucket
+    blob_name = "https://storage.cloud.google.com/datos_web_audio_testigos/prorgramas/GenAudTes_Installer.exe"  # ruta dentro del bucket
 
     signed_url = generate_download_signed_url(bucket, blob_name)
 
