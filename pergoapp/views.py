09839b7e22713f8c90d0d0a8ef5decee9df842db
NandoXa35 <nando300515@gmail.com>
2025-06-08 11:38:22 -0600

    agregar funcion para generar archivos de audio

--- pergoapp/views.py
+++ pergoapp/views.py
@@ -373,10 +373,19 @@ def _ejecutar_job(usuario, bucket, siglas, fecha):
     """
     try:
 
+        # 1️⃣  Convertir a dict si es string
+        cred_info = (settings.GS_CREDENCIALS
+                     if isinstance(settings.GS_CREDENCIALS, dict)
+                     else json.loads(settings.GS_CREDENCIALS))
+
+        creds = service_account.Credentials.from_service_account_info(cred_info)
+
+        # 2️⃣  Vertex AI ► init con creds
         aiplatform.init(
             project=settings.GCP_PROJECT_ID,
             location="us-central1",
-            staging_bucket="gs://nimble-vertex-staging"
+            staging_bucket="gs://nimble-vertex-staging",
+            credentials=creds,  # ← aquí va
         )
 
         IMAGE_URI = "us-central1-docker.pkg.dev/nimble-depot-456123-n0/vertex-training/audio-training-cu124-corregido:v1"
